a4a0615ea15807b0deba26f8dd13e72fe4dee18a
Chaitali1984 <104355290+Chaitali1984@users.noreply.github.com>
2022-07-31 15:21:52 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -13,8 +13,6 @@ def user_input_features():
 
    data = {'price'}
 
- features = pd.DataFrame(data,index = [0])
-          return features 
 
    df = user_input_features()
    st.subheader('User Input parameters')
